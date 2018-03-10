e2b011c125d2043333474b4b59248ea5bf0cda56
Ruben Baskaran <rbaskaran92@gmail.com>
2018-03-10 20:37:39 +0100

    Removed Test

--- app/src/main/java/rubenbaskaran/com/datarecorderapp/MainActivity.java
+++ app/src/main/java/rubenbaskaran/com/datarecorderapp/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        //Test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
